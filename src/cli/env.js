cf2f98720e93bbfb345de287095c2ccab5dd2e83
eys-man <kapuec2010@gmail.com>
2024-10-06 00:58:39 +0300

    feat: add CLI functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const EnvVariables = Object.entries(process.env);
+
+    const rssVariables = [];
+    EnvVariables.forEach(([key, value]) => {
+        if (key.startsWith('RSS_'))
+            rssVariables.push(`${key}=${value}`);
+    });
+    console.log(rssVariables.join('; '));
 };
 
 parseEnv();
