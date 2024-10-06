c5143438555a8dea7800fd8d769293e5ba3a75ea
eys-man <kapuec2010@gmail.com>
2024-10-07 01:04:54 +0300

    feat: add stream functions - read, write and transform

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const writeStream = createWriteStream(
+            path.resolve(__dirname, './files/fileToWrite.txt'),
+            { flags: 'a', encoding: 'utf-8' }
+        );
+        await pipeline(process.stdin, writeStream);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await write();
