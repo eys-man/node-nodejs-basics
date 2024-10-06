6b46eb84a0fd0b428366102d20f8d9a92cfcf294
eys-man <kapuec2010@gmail.com>
2024-10-07 01:19:20 +0300

    feat: add compress & decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const readStream = createReadStream(path.resolve(__dirname, './files/fileToCompress.txt'));
+        const writeStream = createWriteStream(path.resolve(__dirname, './files/archive.gz'));
+
+        await pipeline(readStream, createGzip(), writeStream);
+    } catch {
+        throw new Error(`FS operation failed`);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createUnzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const readStream = createReadStream(path.resolve(__dirname, './files/archive.gz'));
+        const writeStream = createWriteStream(path.resolve(__dirname, './files/fileToCompress.txt'));
+
+        await pipeline(readStream, createUnzip(), writeStream);
+    } catch {
+        throw new Error(`FS operation failed`);
+    }
 };
 
 await decompress();
